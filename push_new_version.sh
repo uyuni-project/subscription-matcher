4c967391f94d6222bbdc9ed3c87ed1dc7d2dc348
Silvio Moioli <smoioli@suse.de>
2015-11-11 11:24:42 +0100

    Added a script to bump versions

---
+++ push_new_version.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage: $0 <VERSION_NUMBER>"
+    exit 1
+fi
+
+mvn versions:set -DnewVersion=$1 scm:add -Dincludes=pom.xml scm:checkin -Dmessage="Version bumped to v$1" 
+mvn scm:tag -Dtag=v$1 -DpushChanges=true
+
