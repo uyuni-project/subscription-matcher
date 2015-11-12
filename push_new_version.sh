afe01f7caadb1883bdd0af49e0abec9e21a228c1
Silvio Moioli <smoioli@suse.de>
2015-11-12 10:11:53 +0100

    push_new_version.sh: don't back up pomfile, we have Git for that

--- push_new_version.sh
+++ push_new_version.sh
@@ -6,6 +6,6 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
-mvn versions:set -DnewVersion=$1 scm:add -Dincludes=pom.xml scm:checkin -Dmessage="Version bumped to v$1" 
+mvn versions:set -DnewVersion=$1 scm:add -Dincludes=pom.xml scm:checkin -Dmessage="Version bumped to v$1" -DgenerateBackupPoms=false
 mvn scm:tag -Dtag=v$1 -DpushChanges=true
 
