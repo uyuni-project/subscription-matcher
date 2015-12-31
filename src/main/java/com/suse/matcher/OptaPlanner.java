01396aa10c45da22033e083a39adecf9cc733b51
Silvio Moioli <smoioli@suse.de>
2015-12-31 10:29:44 +0100

    Configure Optaplanner in code and not via an XML file
    This should be easier to comprehend - all OptaPlanner related
    stuff is in the same file and using Java is easier to
    use comments to explain what's going on.
    
    This also opens the door to dynamically change the configuration
    (eg. to have different settings for tests).

--- src/main/java/com/suse/matcher/OptaPlanner.java
+++ src/main/java/com/suse/matcher/OptaPlanner.java
@@ -1,16 +1,34 @@
 package com.suse.matcher;
 
 import com.suse.matcher.solver.Assignment;
+import com.suse.matcher.solver.Match;
 
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
+import org.optaplanner.core.config.constructionheuristic.ConstructionHeuristicPhaseConfig;
+import org.optaplanner.core.config.constructionheuristic.ConstructionHeuristicType;
+import org.optaplanner.core.config.heuristic.selector.move.MoveSelectorConfig;
+import org.optaplanner.core.config.heuristic.selector.move.composite.CartesianProductMoveSelectorConfig;
+import org.optaplanner.core.config.heuristic.selector.move.composite.UnionMoveSelectorConfig;
+import org.optaplanner.core.config.heuristic.selector.move.generic.ChangeMoveSelectorConfig;
+import org.optaplanner.core.config.localsearch.LocalSearchPhaseConfig;
+import org.optaplanner.core.config.localsearch.decider.acceptor.AcceptorConfig;
+import org.optaplanner.core.config.localsearch.decider.forager.LocalSearchForagerConfig;
+import org.optaplanner.core.config.score.definition.ScoreDefinitionType;
+import org.optaplanner.core.config.score.director.ScoreDirectorFactoryConfig;
+import org.optaplanner.core.config.solver.EnvironmentMode;
+import org.optaplanner.core.config.solver.SolverConfig;
+import org.optaplanner.core.config.solver.random.RandomType;
+import org.optaplanner.core.config.solver.termination.TerminationConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
 
 /**
  * Facade on the OptaPlanner solver.
  *
- * Fills a Solution object based on configuration specified in solverConfig.xml.
+ * Fills a Solution object.
  */
 public class OptaPlanner {
 
@@ -33,8 +51,7 @@ public class OptaPlanner {
         }
 
         // init solver
-        SolverFactory solverFactory = SolverFactory.createFromXmlResource("solverConfig.xml");
-        Solver solver = solverFactory.buildSolver();
+        Solver solver = initSolver();
 
         // solve problem
         long start = System.currentTimeMillis();
@@ -42,6 +59,131 @@ public class OptaPlanner {
         logger.info("Optimization phase took {}ms", System.currentTimeMillis() - start);
         result = (Assignment) solver.getBestSolution();
         logger.info("{} matches confirmed", result.getMatches().stream().filter(m -> m.confirmed).count());
+    }
+
+    /**
+     * Configures and returns an OptaPlanner solver.
+     *
+     * This method replaces the XML configuration file cited in Optaplanner's documentation.
+     *
+     * @return the solver
+     */
+    private Solver initSolver() {
+        // init basic objects
+        SolverFactory factory = SolverFactory.createEmpty();
+        SolverConfig config = factory.getSolverConfig();
+        config.setPhaseConfigList(new ArrayList<>());
+
+        /*
+         * Ensure results are reproducible across runs and JVMs
+         */
+        config.setEnvironmentMode(EnvironmentMode.REPRODUCIBLE);
+        config.setRandomType(RandomType.MERSENNE_TWISTER);
+        config.setRandomSeed(0L);
+
+        /*
+         * Declare solution and entity classes
+         */
+        config.setSolutionClass(Assignment.class);
+        config.setEntityClassList(new ArrayList<Class<?>>(){{ add(Match.class); }});
+
+        /*
+         * Declare score type and file location
+         */
+        ScoreDirectorFactoryConfig score = new ScoreDirectorFactoryConfig();
+        score.setScoreDefinitionType(ScoreDefinitionType.HARD_SOFT);
+        score.setScoreDrlList(new ArrayList<String>() {{ add("com/suse/matcher/rules/optaplanner/Scores.drl"); }});
+        config.setScoreDirectorFactoryConfig(score);
+
+        /*
+         * Construct an initial solution by visiting all possible Matches one by one, in order,
+         * and changing that Match's confirmed property from the initial null value first to true and
+         * then to false. Take whichever of the two has higher score and move on to the next Match
+         * (jargon for this is "first fit").
+         *
+         * Because of how the score is calculated, moving a Match's confirmed property from null to either
+         * true or false can only make hard score go down and/or the soft score go up (see Scores.drl).
+         *
+         * At the end of this process (called a Construction Heuristic or CH step) the hard score cannot
+         * be negative and the soft score is typically positive (worst case is all Match.confirmed being set to
+         * false, which yields 0/0).
+         */
+        ConstructionHeuristicPhaseConfig contructionHeuristic = new ConstructionHeuristicPhaseConfig();
+        contructionHeuristic.setConstructionHeuristicType(ConstructionHeuristicType.FIRST_FIT);
+        config.getPhaseConfigList().add(contructionHeuristic);
+        score.setInitializingScoreTrend("ONLY_DOWN/ONLY_UP");
+
+        /*
+         * Starting from the initial solution from the CH phase, explore other solutions by flipping some
+         * Match.confirmed boolean values. A change from a certain solution to a new solution is called a move,
+         * moves are repeated in iterations called steps.
+         *
+         * Sequences of steps will hopefully get to some new solutions that have a better score.
+         *
+         * We allow moves to change from 1 to 4 Match.confirmed values per step.
+         */
+        LocalSearchPhaseConfig search = new LocalSearchPhaseConfig();
+        UnionMoveSelectorConfig move = new UnionMoveSelectorConfig(new ArrayList<>());
+        move.getMoveSelectorConfigList().add(new ChangeMoveSelectorConfig());
+        move.getMoveSelectorConfigList().add(new CartesianProductMoveSelectorConfig(new ArrayList<MoveSelectorConfig>() {{
+            add(new ChangeMoveSelectorConfig());
+            add(new ChangeMoveSelectorConfig());
+        }}));
+        move.getMoveSelectorConfigList().add(new CartesianProductMoveSelectorConfig(new ArrayList<MoveSelectorConfig>() {{
+            add(new ChangeMoveSelectorConfig());
+            add(new ChangeMoveSelectorConfig());
+            add(new ChangeMoveSelectorConfig());
+        }}));
+        move.getMoveSelectorConfigList().add(new CartesianProductMoveSelectorConfig(new ArrayList<MoveSelectorConfig>() {{
+            add(new ChangeMoveSelectorConfig());
+            add(new ChangeMoveSelectorConfig());
+            add(new ChangeMoveSelectorConfig());
+            add(new ChangeMoveSelectorConfig());
+        }}));
+
+        /*
+         * Every step, generate several moves and pick the best scoring one as the next step.
+         *
+         * As possible moves might be a lot, don't generate more than 20_000 in any case.
+         */
+        move.setSelectedCountLimit(20_000L);
+        search.setMoveSelectorConfig(move);
+
+        /*
+         * Among generated moves, don't accept moves:
+         *   - that do not actually change the solution (eg. true -> true)
+         *   - that make the hard score negative
+         *   - that were already attempted in the last 50 steps (colloquially called "taboo").
+         *
+         * This prevents us to run in circles, repeating the same moves over and over (provided
+         * the circle is shorter than 50 steps).
+         */
+        AcceptorConfig acceptor = new AcceptorConfig();
+        acceptor.setMoveTabuSize(50);
+        search.setAcceptorConfig(acceptor);
+
+        /*
+         * As accepted moves might still be a lot, don't evaluate more than 10_000 in any case.
+         */
+        LocalSearchForagerConfig forager = new LocalSearchForagerConfig();
+        forager.setAcceptedCountLimit(10_000);
+        search.setForagerConfig(forager);
+
+        /*
+         * Continue stepping and keep track of the overall best solution found so far.
+         *
+         * At some point we have to stop stepping, and we do so when:
+         *   - we stepped 500 times with no score improvement (typically)
+         *   - we stepped 10_000 times (when all else fails)
+         */
+        TerminationConfig termination = new TerminationConfig();
+        termination.setUnimprovedStepCountLimit(500);
+        termination.setStepCountLimit(10_000);
+        search.setTerminationConfig(termination);
+
+        // return solver
+        config.getPhaseConfigList().add(search);
+        return factory.buildSolver();
     }
 
     /**
