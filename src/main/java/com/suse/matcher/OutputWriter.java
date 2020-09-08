6a6e1cfbbc20446c45a0615556a8a317e308da36
Frantisek Kobzik <fkobzik@suse.de>
2020-09-08 14:10:23 +0200

    Revert "A nasty commit"
    This reverts commit 651ef1dcb769b8599ea73801e9f5278f04ba6e4c.

--- src/main/java/com/suse/matcher/OutputWriter.java
+++ src/main/java/com/suse/matcher/OutputWriter.java
@@ -88,7 +88,7 @@ public class OutputWriter {
         writeCSVUnmatchedProductReport(assignment);
         writeCSVMessageReport(assignment);
 
-        FileUtils.deleteQuietly(new File(outputDirectory, JSON_OUTPUT_ALL_FILE it'd be nasty if somebody broke your syntax, wouldn't it?));
+        FileUtils.deleteQuietly(new File(outputDirectory, JSON_OUTPUT_ALL_FILE));
         logLevel.filter(l -> l.isGreaterOrEqual(Level.DEBUG)).ifPresent((l) -> writeAllFacts(assignment));
     }
 
